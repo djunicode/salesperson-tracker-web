569f4e8877724dacc31dd3f41ed5b14acbb8ca44
VirangParekh <44228173+VirangParekh@users.noreply.github.com>
2020-04-10 20:38:49 +0530

    Views Done

--- salespersonTrackerREST/models.py
+++ salespersonTrackerREST/models.py
@@ -1,43 +1,107 @@
 from django.db import models
-from . import media
 from django.contrib.auth.models import User
 
-class Manager(models.Model):
-    pass
-
-class SalesPerson(models.Model):
-    username=models.OneToOneField(User, on_delete=models.CASCADE)
-    ref_id=models.IntegerField(primary_key=True, verbose_name='salesperson_id')
-    name=models.CharField(verbose_name='salesperson_name',max_length=250)
-    age=models.IntegerField(verbose_name='salesperson_age')
-    photo=models.ImageField(verbose_name='salesperson_photo',upload_to='media')
-    lat=models.FloatField(verbose_name='latitude')
-    lon=models.FloatField(verbose_name='longitude')
-    manager=models.ForeignKey(Manager, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.ref_id
-
-class CheckPoints(models.Model):
-    unique_id=models.IntegerField(primary_key=True, verbose_name='checkpoint_id')
-    lat_check=models.FloatField(verbose_name='latitude')
-    lon_check=models.FloatField(verbose_name='longitude')
-    salesperson=models.ForeignKey(SalesPerson, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.unique_id
-
-class Stock(models.Model):
-    stock_id=models.IntegerField(primary_key=True,verbose_name='stock_uniqueid')
-    sales_person=models.ForeignKey(SalesPerson, on_delete=models.CASCADE)
-    stock_name=models.CharField(max_length=250)
-    stock_type=models.CharField(max_length=120)
-
-    def __str__(self):
-        return self.stock_id
-    
-
-
-class Item(models.Model):
-    pass
 # Create your models here.
+# Using Custom Django User Model
+# Managers be created via the admin only
+# Initially give managers same  password give them access to reset password
+# username:employee_id
+
+
+class Manager(models.Model):
+    user_ref = models.ForeignKey(User, on_delete=models.CASCADE)
+    Name = models.CharField(max_length=100)
+    Photo = models.ImageField(upload_to="managers")
+    Age = models.IntegerField()
+
+    def __str__(self):
+        return self.user_ref.username
+
+
+# Salesperson will also be created via the admin only
+# give salesperson the same password provide a link to reset their passwords
+# username:employee_id
+
+
+class Salesperson(models.Model):
+    User_ref = models.ForeignKey(User, on_delete=models.CASCADE)
+    Managed_By = models.ForeignKey(Manager, models.SET_NULL, null=True, blank=True)
+    Name = models.CharField(max_length=100)
+    Photo = models.ImageField(upload_to="salesperson")
+    Age = models.IntegerField()
+    last_location_lat = models.FloatField()
+    last_location_long = models.FloatField()
+
+    def __str__(self):
+        return self.User_ref.username
+
+
+# Item_Group_Code:
+# Classify items into their respective domains,like for eg a Toshiba Tv and a Samsung Tv can be grouped togheter under one roof named TV
+# Company_item_code:A company can produce many diffrent models of Tv,hence to classify these models under one Manufacturer Equipment
+# They will be diffrent from each other by their item_code(i.e Barcode)
+
+
+# Item model is more a warehouse,where the mangers can view these items and assign them to the salesperson
+class Item(models.Model): #R
+    Item_Code = models.IntegerField(primary_key=True)
+    Item_Group_Code = models.IntegerField()
+    Company_Item_code = models.IntegerField()
+    Company_Code = models.IntegerField()
+    Photo = models.ImageField(upload_to="Item")
+    Description = models.TextField()
+
+    def __int__(self):
+        return self.Item_Code
+
+
+class ItemAssign(models.Model):
+    Item_Ref = models.ForeignKey(Item, on_delete=models.CASCADE)
+    Assigned_By = models.ForeignKey(Manager, models.SET_NULL, null=True, blank=True)
+    Assigned_To = models.ForeignKey(Salesperson, models.SET_NULL, null=True, blank=True)
+    Assign_Date = models.DateField()
+    Assign_Time = models.TimeField()
+
+    def __str__(self):
+        return self.Item_Ref
+
+
+class Inventory(models.Model):  #R 
+    Salesperson_Ref = models.ForeignKey(Salesperson, on_delete=models.CASCADE)
+    item_Ref = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Salesperson_Ref
+
+
+class Bill(models.Model):
+    Item_Ref = models.ForeignKey(Item, on_delete=models.CASCADE)
+    Issued_To = models.CharField(max_length=100)
+    Salesperson_Ref = models.ForeignKey(
+        Salesperson, models.SET_NULL, null=True, blank=True
+    )
+
+    Buyer_Contact = models.IntegerField()
+    Buyer_email = models.CharField(max_length=100)
+    SoftCopy = models.FileField(upload_to="Bills")
+
+    def __str__(self):
+        return self.Item_Ref 
+
+class DailyTarget(models.Model):
+    Assigned_By = models.ForeignKey(Manager, models.SET_NULL, blank=True, null=True)
+    Assigned_To = models.ForeignKey(Salesperson, on_delete=models.CASCADE)
+    Assigned_Date = models.DateField()
+    Assigned_Time = models.TimeField()
+    Item_Ref = models.ForeignKey(Inventory, on_delete=models.CASCADE)
+    Quantity = models.IntegerField()
+    Completed = models.BooleanField()
+    Notes = models.TextField()
+
+class TargetsCompleted(models.Model):
+    User_ref = models.ForeignKey(Manager, models.SET_NULL, null=True)
+    Task_completed = models.IntegerField()
+
+class TotalTargets(models.Model):   #R
+    User_ref = models.ForeignKey(Manager, models.SET_NULL, null=True)
+    Task_Assigned = models.IntegerField()
---
+++ salespersonTrackerREST/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import DailyTarget, TargetsCompleted, Bill
+
+
+class DailyTargetSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=DailyTarget
+        fields= '__all__'
+
+
+class TargetsCompletedSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=TargetsCompleted
+        fields='__all__'
+
+class BillSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=Bill
+        fields='__all__'
