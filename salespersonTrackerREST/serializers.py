8e4372702f24a5271ba6c696c15fb191dd360c12
ali7070droid <aakanadia@gmail.com>
2020-04-28 21:40:10 +0530

    Adding item to inventory in dict

--- salespersonTrackerREST/serializers.py
+++ salespersonTrackerREST/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth.models import User
 
 
 class BillSerializer(serializers.ModelSerializer):
@@ -12,6 +13,30 @@ class DailyTargetSerializer(serializers.ModelSerializer):
     class Meta:
         model = DailyTarget
         fields = "__all__"
+
+
+class Userserializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username"]
+
+
+class SalespersonSerializer(serializers.ModelSerializer):
+    User_ref = Userserializer(many=False, read_only=True)
+
+    class Meta:
+        model = Salesperson
+        fields = [
+            "id",
+            "User_ref",
+            "Managed_By",
+            "Name",
+            "Photo",
+            "Age",
+            "last_location_lat",
+            "last_location_long",
+            "isLoggedin",
+        ]
 
 
 class ItemAssignSerializer(serializers.ModelSerializer):
