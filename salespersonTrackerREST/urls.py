a063432825cedba7db610441c5f4d96a62857d92
Jash271 <shahjash271@gmail.com>
2020-05-01 23:05:45 +0530

    Populate Database and update coordinates endpoint added

--- salespersonTrackerREST/urls.py
+++ salespersonTrackerREST/urls.py
@@ -3,23 +3,15 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from .views import (
-    DailyTargetView,
-    BillView,
-    SignIn,
-    ChangePassword,
-    Logout,
-    WarehouseViewSet,
-    ItemAssignViewSet,
-)
+
 from rest_framework import routers
 
 
 router = routers.DefaultRouter()
-router.register(r"daily_target", DailyTargetView, basename="daily_target")
-router.register(r"bill", BillView, basename="bill")
+router.register(r"daily_target", views.DailyTargetView, basename="daily_target")
+router.register(r"bill", views.BillView, basename="bill")
 # router.register(r'targets_completed', TargetsCompletedView)
-router.register("items", WarehouseViewSet)
+router.register("items", views.WarehouseViewSet)
 
 
 urlpatterns = [
@@ -31,7 +23,7 @@ urlpatterns = [
     path("GetCoordinates", views.GetCoordinates.as_view(), name="GetCoordinates"),
     path(
         "GetSalespersonData", views.SalespersonData.as_view(), name="GetSalespersonData"
-    )
+    ),
     # path("Test", views.Test, name="Test"),
     # path("accept", views.accept, name="accept"),
     # path('daily_target',DailyTargetView.as_view(),name='daily_target'),
@@ -42,6 +34,9 @@ urlpatterns = [
         name="AddToInventory",
     ),
     path("InventoryList", views.InventoryList.as_view(), name="InventoryList"),
+    path("ManagerPopulate", views.ManagerPopulate, name="ManagerPopulate"),
+    path("SalespersonPopulate", views.SalespersonPopulate, name="SalespersonPopulate"),
+    #path("UpdateCoordinates", views.UpdateCoordinates.as_view(), name="UpdateCoordinates"),
 ]
 
 urlpatterns += router.urls
