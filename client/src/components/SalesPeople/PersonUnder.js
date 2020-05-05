3587d6cb61117e8688c01ac0be6253aac67ade57
shrey-958 <shreydedhia@yahoo.in>
2020-05-05 13:19:13 +0530

    Displayed SalesPeople Details

--- client/src/components/SalesPeople/PersonUnder.js
+++ client/src/components/SalesPeople/PersonUnder.js
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import './PersUnd.css'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Avatar from '@material-ui/core/Avatar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
@@ -18,6 +24,7 @@ import Select from '@material-ui/core/Select';
 // import Box from '@material-ui/core/Box';
 // import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 // import Paper from '@material-ui/core/Paper';
+
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -52,9 +59,45 @@ const useStyles = makeStyles((theme) => ({
     height: '60px',
     backgroundColor: 'grey',
   },
+  expanded: {
+    margin: "0 auto"
+  },
 }));
 
 var PersonUnder = () => {
+  
+  var [ListPeople, listing] = useState([]) 
+ 
+  
+
+ 
+  
+  
+  
+  useEffect( () => {
+    const word = 'Token ';
+  const token = word.concat(`${localStorage.getItem('Token')}`);
+  let formData = new FormData();
+  formData.append('Authorization', `${token}`);
+    fetch(`http://127.0.0.1:8000/Operations/GetSalespersonData`, {
+      method: 'GET',
+      headers: formData
+    })
+      .then((res) => res.json())
+      .then((data) => {
+       var myPeople = data.filter(d => d.User_ref.username == 'admin2');
+       console.log(myPeople)
+       listing(myPeople)
+       console.log(ListPeople)
+       
+       
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+},[])
+
+
   const classes = useStyles();
   const [items, setItems] = React.useState('');
   const [locations, setLocations] = React.useState('');
@@ -66,6 +109,14 @@ var PersonUnder = () => {
     setLocations(event.target.value);
   };
 
+  
+
+  
+ 
+
+ 
+  
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -73,82 +124,62 @@ var PersonUnder = () => {
       <form className={classes.form} noValidate>
         <Grid container spacing={2}>
           <Grid item xs={10}>
-            <Typography component="h5" variant="h7" align="center">
-              SALES PERSON UNDER YOU{' '}
+            <Typography component="h5" variant="h7" align="center" style={{fontSize : 
+
+'115%', fontFamily: 'Segoe UI'}}>
+              SALES PEOPLE UNDER YOU{' '}
             </Typography>{' '}
           </Grid>
-          <Grid item xs={3}>
-            <Avatar variant="square" className={classes.square}>
-              N{' '}
-            </Avatar>{' '}
-          </Grid>
-          <Grid item xs={7}>
-            <Typography>NAME </Typography> <Typography>DETAILS </Typography>{' '}
-          </Grid>
-          <Grid item xs={10}>
-            <Typography component="h5" variant="h6" align="center">
-              DETAILS{' '}
-            </Typography>{' '}
-          </Grid>
-          <Grid item xs={10}>
-            <Typography component="h5" variant="h6" align="center">
-              DETAILS{' '}
-            </Typography>{' '}
-          </Grid>
-          <Grid item xs={10}>
-            <FormControl className={classes.formControl} fullWidth>
-              <InputLabel id="items-associated"> SET CHECKPOINTS </InputLabel>{' '}
-              <Select
-                //labelId="demo-simple-select-label"
-                id="items-associated"
-                value={items}
-                onChange={handleChangeItem}
-              >
-                <MenuItem value={10}> OPTION 1 </MenuItem>{' '}
-                <MenuItem value={20}> OPTION 2 </MenuItem>{' '}
-                <MenuItem value={30}> OPTION 3 </MenuItem>{' '}
-              </Select>{' '}
-            </FormControl>{' '}
-          </Grid>{' '}
-          <Grid item xs={10}>
-            <FormControl className={classes.formControl} fullWidth>
-              <InputLabel id="demo-simple-select-label">
-                {' '}
-                SET TARGET{' '}
-              </InputLabel>{' '}
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={locations}
-                onChange={handleChangeLocation}
-              >
-                <MenuItem value={10}> Ten </MenuItem>{' '}
-                <MenuItem value={20}> Twenty </MenuItem>{' '}
-                <MenuItem value={30}> Thirty </MenuItem>{' '}
-              </Select>{' '}
-            </FormControl>{' '}
-          </Grid>{' '}
+          
+          
+          {ListPeople.length == 0
+          ? <div>Loading People...</div>
+          :<Grid item xs={10}> 
+      {ListPeople.map((value) => {
+        return (
+        <ExpansionPanel className="panel" style={{width : '150%', height : '100px', 
+
+marginBottom : '55px', marginLeft:'-35%'}} className={classes.expanded}>
+        
+        <ExpansionPanelSummary style={{marginBottom : '-65px'}} expandIcon=
+
+{<ExpandMoreIcon />}>
+        <div class="inf-container" ><img class="prof-pic" src = {value.Photo} />
+        <div class="prim-details"><span class= 'dot' style={ value.isLoggedin ? { 
+
+backgroundColor:'green'} : {backgroundColor : '#bbb'} }  ></span> <div class="per-
+
+det"> <b>{value.Name}</b>,  {value.Age}</div>
+        
+        </div> </div>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails style={{height:'5px',backgroundColor:'#cf5504', 
+
+marginBottom:'0%'}}>
+          <div class="coordinates">LAT - {value.last_location_lat} </div>
+          <div class="coordinates">LONG - {value.last_location_long} </div>
+        </ExpansionPanelDetails>
+       
+        </ExpansionPanel>
+        )
+      })}
+    </Grid>
+          }
+         
+    
+         
+          
+          
+          
+          
           <br />
           <br />
           <br />
           <br />
-          <Grid item xs={3}>
-            <Avatar variant="square" className={classes.square}>
-              T{' '}
-            </Avatar>{' '}
-          </Grid>
-          <Grid item xs={7}>
-            <Typography>TARGET </Typography>{' '}
-          </Grid>
-          <Grid item xs={3}>
-            <Avatar variant="square" className={classes.square}>
-              T{' '}
-            </Avatar>{' '}
-          </Grid>
-          <Grid item xs={7}>
-            <Typography>TARGET </Typography>{' '}
-          </Grid>{' '}
-        </Grid>{' '}
+          
+          
+            
+        </Grid>
       </form>
     </Container>
   );
